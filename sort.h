964e2934aa67d6ccbd9ba3d0afcb4eaceeedfd33
Liam190100 <jnnjuguna8@gmail.com>
2023-09-23 03:17:47 -0700

    sort

--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ typedef struct listint_s
 } listint_t;
 
 void print_array(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void swap_int(int *a, int *b);
 void insertion_sort_list(listint_t **list);
