354cc88f279cc3fb11138fe652b5e4ec59c7d93f
Liam190100 <jnnjuguna8@gmail.com>
2023-09-23 08:18:12 -0700

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,32 +7,39 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = (*list)->next;
-	listint_t *prev_node;
+	listint_t *current, *prev_node;
 
 	if (!list || !(*list) || !(*list)->next)
-	return;
+		return;
+
+	current = (*list)->next;
 
 	while (current)
 	{
-		prev_node = current->prev;
-		while (prev_node && current->n < prev_node->n)
-		{
-			if (prev_node->prev)
-			prev_node->prev->next = current;
-			if (current->next)
-			current->next->prev = prev_node;
-			prev_node->next = current->next;
-			current->prev = prev_node->prev;
-			prev_node->prev = current;
-			current->next = prev_node;
+	prev_node = current->prev;
 
-			if (!current->prev)
+	while (prev_node && current->n < prev_node->n)
+	{
+		if (prev_node->prev)
+		prev_node->prev->next = current;
+
+		if (current->next)
+		current->next->prev = prev_node;
+
+		prev_node->next = current->next;
+		current->prev = prev_node->prev;
+		prev_node->prev = current;
+		current->next = prev_node;
+
+		if (!current->prev)
 			*list = current;
 
-			print_list(*list);
-			prev_node = current->prev;
-		}
-		current = current->next;
+		print_list(*list);
+
+		prev_node = current->prev;
+	}
+
+	current = current->next;
 	}
 }
+
